5e9d3c72b387254692396f49a54f77bb9b87e869
Guillaume <108530065+baldurstod@users.noreply.github.com>
2024-08-20 01:19:45 +0200

    Added moving lights

--- src/client/js/demos/materials/pbr/spheres.js
+++ src/client/js/demos/materials/pbr/spheres.js
@@ -17,6 +17,24 @@ export function initDemo(renderer, scene) {
 	const metalnessText = new Harmony3D.Text3D({ text: 'metalness', size: 5, depth: 1, position: [-20, 0, -35], scale: [1, 1, 0.1], parent: scene });
 	const roughnessText = new Harmony3D.Text3D({ text: 'roughness', size: 5, depth: 1, position: [-35, 0, -20], quaternion: [0.5, -0.5, 0.5, 0.5], scale: [1, 1, 0.1], parent: scene });
 
+
+	const lights = [];
+	const teta = [];
+	const speed = [];
+
+	for (let i = 0; i < 4; i++) {
+		let l = new Harmony3D.PointLight({ position: [0, -100, -0], intensity: 0.25, parent: scene });
+		lights.push(l);
+		teta.push(Math.random() * 2 * Math.PI);
+		speed.push(Math.random() * 2 - 1);
+	}
+/*
+	let pl0 = new Harmony3D.PointLight({ position: [-100, -100, -100], intensity: 0.25, parent: scene });
+	let pl1 = new Harmony3D.PointLight({ position: [-100, -100, +100], intensity: 0.25, parent: scene });
+	let pl2 = new Harmony3D.PointLight({ position: [+100, -100, -100], intensity: 0.25, parent: scene });
+	let pl3 = new Harmony3D.PointLight({ position: [+100, -100, +100], intensity: 0.25, parent: scene });
+	*/
+
 	for (let i = 0; i <= 10; i++) {
 		for (let j = 0; j <= 10; j++) {
 			let material = new Harmony3D.MeshBasicPbrMaterial({ metalness: i / 10, roughness: j / 10 });
@@ -25,4 +43,13 @@ export function initDemo(renderer, scene) {
 			group.addChild(sphere);
 		}
 	}
+
+	Harmony3D.GraphicsEvents.addEventListener(Harmony3D.GRAPHICS_EVENT_TICK, (event) => {
+		//orthoCameraControl.update(event.detail.delta / 1000)
+
+		for (let i = 0; i < 4; i++) {
+			let angle = 4 * speed[i] * event.detail.time / 1000. + teta[i];
+			lights[i].position = [100 * Math.cos(angle), -100, 100 * Math.sin(angle)];
+		}
+	});
 }
