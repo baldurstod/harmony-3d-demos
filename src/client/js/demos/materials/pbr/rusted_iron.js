9f26b993adfc0cb21f6fce8461c4c7eef1cfc866
Guillaume <108530065+baldurstod@users.noreply.github.com>
2024-08-24 18:04:47 +0200

    Updated scene params

--- src/client/js/demos/materials/pbr/rusted_iron.js
+++ src/client/js/demos/materials/pbr/rusted_iron.js
@@ -5,13 +5,13 @@ let perspectiveCamera;
 let orbitCameraControl;
 export function initDemo(renderer, scene) {
 	[perspectiveCamera, orbitCameraControl] = InitDemoStd(renderer, scene);
-	perspectiveCamera.position = [0, -100, 0];
+	perspectiveCamera.position = [0, -10, 0];
 	orbitCameraControl.target.position = [0, 0, 0];
-	perspectiveCamera.farPlane = 10000;
-	perspectiveCamera.nearPlane = 1;
+	perspectiveCamera.farPlane = 1000;
+	perspectiveCamera.nearPlane = 0.1;
 	perspectiveCamera.verticalFov = 50;
 	renderer.clearColor(GlMatrix.vec4.fromValues(0.1, 0.1, 0.1, 255));
 
-	let l = new Harmony3D.PointLight({ position: [0, -100, -0], intensity: 2, parent: scene });
-	new Harmony3D.Sphere({ parent: scene, radius: 10, material: createPbrMaterial('rustediron2'), segments: 32, rings: 32 });
+	let l = new Harmony3D.PointLight({ position: [0, -2, -0], intensity: 1, parent: scene });
+	new Harmony3D.Sphere({ parent: scene, radius: 1, material: createPbrMaterial('rustediron2'), segments: 32, rings: 32 });
 }
