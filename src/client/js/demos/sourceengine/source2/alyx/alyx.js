2a9db79e2b21de9b0fbdd64560eac1443ada99f3
Guillaume <108530065+baldurstod@users.noreply.github.com>
2024-09-01 22:26:04 +0200

    Added Alyx

---
+++ src/client/js/demos/sourceengine/source2/alyx/alyx.js
@@ -0,0 +1,23 @@
+import { AddSource2Model, InitDemoStd, Harmony3D, GlMatrix } from '/js/application.js';
+
+let perspectiveCamera;
+let orbitCameraControl;
+let ambientLight;
+export function initDemo(renderer, scene) {
+	[perspectiveCamera, orbitCameraControl, ambientLight] = InitDemoStd(renderer, scene);
+	ambientLight.intensity = 0.2;
+	TestDog(renderer, scene);
+}
+
+async function TestDog(renderer, scene) {
+	perspectiveCamera.position = [1800, 0, 150];
+	orbitCameraControl.target.position = [0, 0, 50];
+	perspectiveCamera.farPlane = 10000;
+	perspectiveCamera.nearPlane = 10;
+	perspectiveCamera.verticalFov = 10;
+	const alyx = await AddSource2Model('hla', 'models/characters/alyx/alyx', renderer, scene);
+	alyx.playAnimation('the_moment_alyx_pre_idle');
+	renderer.clearColor(GlMatrix.vec4.fromValues(0.1, 0.1, 0.1, 255));
+
+	const l = new Harmony3D.PointLight({ position: [0, 150, 0], intensity: 100000, parent: scene });
+}
