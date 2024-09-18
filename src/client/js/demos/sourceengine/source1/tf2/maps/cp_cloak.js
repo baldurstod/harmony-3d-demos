f45fddf44f87537f87ce730bf6578552fe209801
Guillaume <108530065+baldurstod@users.noreply.github.com>
2024-09-18 23:25:31 +0200

    Added cp_cloak

---
+++ src/client/js/demos/sourceengine/source1/tf2/maps/cp_cloak.js
@@ -0,0 +1,56 @@
+import { InitDemoStd, Harmony3D } from '/js/application.js';
+
+let perspectiveCamera;
+let orbitCameraControl;
+let ambientLight;
+export function initDemo(renderer, scene) {
+	[perspectiveCamera, orbitCameraControl, ambientLight] = InitDemoStd(renderer, scene);
+	testMap(renderer, scene);
+}
+
+async function testMap(renderer, scene) {
+	ambientLight.remove();
+	perspectiveCamera.position = [0, 0, 5000];
+	orbitCameraControl.target.position = [0, 0, 0];
+	perspectiveCamera.farPlane = 100000;
+	perspectiveCamera.nearPlane = 100;
+	perspectiveCamera.verticalFov = 50;
+	Harmony3D.Graphics.setIncludeCode('test', '#define PHYSICALLY_CORRECT_LIGHTS');
+
+	//WORKING
+	//cp_dustbowl
+	//itemtest
+	//arena_watchtower
+	//koth_harvest_final
+	//sfm_photostudio_lite
+	//koth_viaduct
+	//cp_cloak
+
+	//MOSTLY WORKS
+	//arena_lumberyard
+
+	//DON'T WORK
+	//pl_badwater
+	//pl_barnblitz
+	//pl_pier
+	//koth_suijin
+	//pl_wutville_event
+	//pl_snowycoast
+	let mapName;
+	mapName = 'maps/cp_cloak.bsp';
+	//mapName = 'maps/itemtest.bsp';
+	//mapName = 'maps/cp_5gorge.bsp';
+
+	let map = await (new Harmony3D.SourceEngineBSPLoader()).load('tf2', mapName);
+	console.error(map);
+	scene.addChild(map);
+	map.initMap();
+
+	/*let heavy = await AddSource1Model('tf2', 'models/player/heavy', renderer, scene);
+	heavy.playSequence('taunt_laugh');*/
+
+	const BOX_SIZE = 100;
+	let box = scene.addChild(new Harmony3D.Box({ width: BOX_SIZE, height: BOX_SIZE, depth: BOX_SIZE, material: new Harmony3D.MeshPhongMaterial() }));
+	box.material.setMeshColor([0.5, 0.5, 0.5, 0.5]);
+	//box.material.renderFace(Harmony3D.RenderFace.Back);
+}
